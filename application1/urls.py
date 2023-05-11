5b5e13988f29a12540125a47e3ecbc59a2e3e5c4
saipavan <saipavan861@gmail.com>
2023-05-11 23:08:20 +0530

    comment removed view

--- application1/urls.py
+++ application1/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('physician',views.index1,name="index1"),
     path('Surgery',views.index2,name="index2"),   
     path('Transaction',views.index3,name="index3"),
-    #path('view',views.index4,name="index4"),
+    path('view',views.index4,name="index4"),
   
 ]
